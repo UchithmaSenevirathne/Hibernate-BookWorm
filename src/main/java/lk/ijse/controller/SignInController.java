728d37303e85c00ee6774a1482b931f839f2f621
UchithmaSenevirathne <uchithma@gmail.com>
2024-02-29 19:25:49 +0530

    Initial project

---
+++ src/main/java/lk/ijse/controller/SignInController.java
@@ -0,0 +1,22 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SignInController {
+    @FXML
+    private AnchorPane signInPane;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+}
---
+++ src/main/java/lk/ijse/controller/SignUpController.java
@@ -0,0 +1,34 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SignUpController {
+    @FXML
+    private AnchorPane signUpPane;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtConfirmPwd;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFullName;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+
+    }
+}
