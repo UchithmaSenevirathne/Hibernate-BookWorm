7d4c4d7ee4970da6fdcc2873ad1ab57c07adf052
UchithmaSenevirathne <uchithma@gmail.com>
2024-02-29 21:04:51 +0530

    update content

--- src/main/java/lk/ijse/controller/SignInController.java
+++ src/main/java/lk/ijse/controller/SignInController.java
@@ -2,8 +2,15 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignInController {
     @FXML
@@ -18,5 +25,21 @@ public class SignInController {
     @FXML
     void btnLoginOnAction(ActionEvent event) {
 
+    }
+
+    @FXML
+    void OpenSignUpOnAction(MouseEvent event) {
+        Parent rootNode = null;
+        try {
+            rootNode = FXMLLoader.load(getClass().getResource("/view/SignUp.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Scene scene = new Scene(rootNode);
+        Stage stage = (Stage) this.signInPane.getScene().getWindow();
+        stage.setTitle("REGISTER");
+        stage.setScene(scene);
+        stage.centerOnScreen();
     }
 }
